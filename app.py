e25daca5c3043f9af70963a95800a3a09b52d40c
duynv4 <duynv4@vng.com.vn>
2025-03-03 23:08:13 +0700

    ok

---
+++ app.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == '__main__':
+    # Use environment variable for port, which Heroku will set
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
